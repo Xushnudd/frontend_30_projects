6a84776384baca1cca8f1925ca3703d9111e41ec
Xushnudd <xushnudnishonov@mail.ru>
2026-01-31 20:52:27 +0500

    door added

---
+++ 20-project/script.js
@@ -0,0 +1,23 @@
+const door = document.querySelector(".door")
+const img = document.querySelector("img")
+
+let a = 0
+
+function open_a_door() {
+    door.classList.remove("close")
+    door.classList.toggle("open")
+}
+function close_a_door() {
+    door.classList.remove("open")
+    door.classList.toggle("close")
+}
+
+function mdoor() {
+    if (a%2 == 0) {
+        open_a_door()
+    }
+    else {
+        close_a_door()
+    }
+    a+=1
+}
